7e474eeff6206d152e62a38d35cf149dcc437acb
劉鎬維 <harvyliu2009@gmail.com>
2025-10-31 16:16:06 +0800

    math and logic

--- math_logic/src/main.rs
+++ math_logic/src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
-  let add = 5 + 3; //add
-  let sub = 10 - 4; //minus
-  let mul = 6 * 2; //multiply
-  let div = 12 / 3; //division
-  let rem = 10 % 3; //reminder
+    let add = 5 + 3; //add
+    let sub = 10 - 4; //minus
+    let mul = 6 * 2; //multiply
+    let div = 12 / 3; //division
+    let rem = 10 % 3; //reminder
 
-  println!("Add: {}", add);
-  println!("Sub: {}", sub);
-  println!("Mul: {}", mul);
-  println!("Div: {}", div);
-  println!("Rem: {}", rem);
+    println!("Add: {}", add);
+    println!("Sub: {}", sub);
+    println!("Mul: {}", mul);
+    println!("Div: {}", div);
+    println!("Rem: {}", rem);
 
 
-  println!("{}", add+2);
+    println!("{}", add+2);
+    let mut x = 10;
+    println!("X before adding: {}", x);
+    x += 1;
+    println!("x after adding: {}", x);
+    
+    let logged_in = true;
+    let is_admin = false;
+
+   println!("Is regular user: {}", logged_in && !is_admin);
+   println!("Has any access: {}", logged_in || is_admin);
+   println!("Not logged in: {}", !logged_in);
 }
+
