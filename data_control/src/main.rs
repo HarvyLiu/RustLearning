4c0df077ca1ff42afa940fc1118468927537ab82
劉鎬維 <harvyliu2009@gmail.com>
2025-10-31 11:20:27 +0800

    upd data_control main.rs learned some datatypes

--- data_control/src/main.rs
+++ data_control/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //regular vars
     let my_num: i32 = 5;          // integer
     let my_double: f64 = 5.99;    // float
     let my_letter: char = 'D';    // character
@@ -9,4 +10,10 @@ fn main() {
               {}\n
               {}\n
               {}\n", my_num, my_double, my_letter, my_bool, my_text);
+    //const e.g:
+    const def_num: i32 = 100; //integer
+    const def_float: f64 = 3.14; //64-bit float
+    const def_str: &str = "This string is unchangable"; //string
+
+    
 }
